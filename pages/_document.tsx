51d3b8bc6335f9506b9b10876dc51f5b191329ad
Manoj <manojkumarprayasjee47@gmail.com>
2022-07-15 06:31:56 -0800

    initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
+const Document = () => {
+  return (
+    <Html className="dark">
+      <Head></Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
