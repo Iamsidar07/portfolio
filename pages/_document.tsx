384eadfe00196d93b1c001aaf594cfb3cc53af82
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-01-21 12:20:04 +0530

    added new projects

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ const Document = () => {
   return (
     <Html className="dark">
       <Head>
-        
+        <link rel="shortcut icon" href="https://tse1.mm.bing.net/th?id=OIP.NwgJqiojY1Z0wg74DmcA4gHaHZ&pid=Api&rs=1&c=1&qlt=95&w=121&h=121" type="image/x-icon" />
       </Head>
       <body>
         <Main />
