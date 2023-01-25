a2025991e34bfbcff56cdfd7e239cf0c7c8a1dc3
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-01-25 21:50:56 +0530

    changed bio

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ const Document = () => {
   return (
     <Html className="dark">
       <Head>
-        <link rel="shortcut icon" href="https://img.freepik.com/free-vector/skull-vintage-stule_225004-1676.jpg?size=338&ext=jpg&ga=GA1.1.755022682.1671260416" type="image/x-icon" />
+        <link rel="shortcut icon" href="https://img.freepik.com/premium-vector/ifrit-demon-illustration_9244-18.jpg?w=2000" type="image/x-icon" />
       </Head>
       <body>
         <Main />
