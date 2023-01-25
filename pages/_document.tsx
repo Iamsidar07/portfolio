ad779f78cbe509f0742b7ac3ff68b0342b1696eb
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-01-26 00:49:42 +0530

    changed simon image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ const Document = () => {
   return (
     <Html className="dark">
       <Head>
-        <link rel="shortcut icon" href="https://img.freepik.com/premium-vector/ifrit-demon-illustration_9244-18.jpg?w=2000" type="image/x-icon" />
+        <link rel="shortcut icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Apostrophe_demon1.jpg/1200px-Apostrophe_demon1.jpg" type="image/x-icon" />
       </Head>
       <body>
         <Main />
